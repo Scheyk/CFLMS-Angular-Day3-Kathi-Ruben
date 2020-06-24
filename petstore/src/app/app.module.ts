98b8a86589a208f747bd860d60a876d4ea64d96c
Ruben <malakasrbn@gmail.com>
2020-06-24 11:31:53 +0200

    ruben folder added

---
+++ petstore/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PetListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
