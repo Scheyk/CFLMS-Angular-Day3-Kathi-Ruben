63eee5cc9c7e180dd2cf70572331bd68864e2c6f
Scheyk <kathrin.dostal@gmail.com>
2020-06-24 13:01:17 +0200

    some trubles mit der ausgabe der prokuckte

--- petShop/src/app/home/home.component.ts
+++ petShop/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { artikel } from '../products';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+	artikel = artikel;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
